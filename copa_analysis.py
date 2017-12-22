6260e2114fc79a21b5af872aae14dc37d01641fa
Claire Herdeman <cherdeman@uchicago.edu>
2017-12-21 22:26:11 -0600

    Loading function updates

--- copa_analysis.py
+++ copa_analysis.py
@@ -5,6 +5,17 @@
 import os
 import pandas as pd
 
+##### CATEGORIES #####
+
+race_cat = ['African American / Black', 'American Indian or Alaskan Native', 
+'Asian or Pacific Islander', 'Hispanic', 'Unknown', 'White']
+
+sex_cat = ['Female', 'Male', 'Unknown']
+
+age_cat = ['0-19', '20-29', '30-39', '40-49', '50-59', '60-69', '70+', 'Unknown']
+
+force_yrs = ['0-4', '5-9', '10-14', '15-19', '20-24', '25-29', '30+', 'Unknown']
+
 ##### LOADING FUNCTIONS #####
 
 def load_full_dataset(filename):
@@ -31,119 +42,31 @@ def load_full_dataset(filename):
 	cols = [cols[0]] + [cols[-1]] + cols[1:len(cols)-2]
 	full_df = full_df[cols]
 
-	# Find number of officers involved, exclude values over 15
-	# This limits excessive row expansion
-	full_df["OFFICER_COUNT"] = full_df["RACE_OF_INVOLVED_OFFICERS"].str.count("\|") + 1
-	limit_officers = full_df[(full_df["OFFICER_COUNT"] <= 15) | (pd.isnull(full_df["OFFICER_COUNT"]))]
-
-	# Split all columns that may have multiple values
-	split_cols = ["BEAT", "RACE_OF_COMPLAINANTS", "SEX_OF_COMPLAINANTS", 
-		"AGE_OF_COMPLAINANTS", "RACE_OF_INVOLVED_OFFICERS", 
-		"SEX_OF_INVOLVED_OFFICERS", "AGE_OF_INVOLVED_OFFICERS", 
-		"YEARS_ON_FORCE_OF_INVOLVED_OFFICERS"]
-	
-	split_df = pd.concat([limit_officers[col].str.split("|", expand = True) for col in split_cols], axis = 1, keys = split_cols)
-	split_df.columns = split_df.columns.map(lambda x: "_".join((x[0], str(x[1]))))
-	
-	# Rejoin with full df
-	new_df = pd.concat([split_df, full_df.drop(split_cols + ["OFFICER_COUNT"], axis = 1)], axis = 1)
-	new_df.fillna(value = Nan, inplace = True)
-	
-	# HIGH LEVEL SUMMARY STATISTICS
-	# Total complaints
-	num_complaints = new_df.shape[0]
-
-	# Year range
-	min_year = new_df["COMPLAINT_YEAR"].min()
-	max_year = new_df["COMPLAINT_YEAR"].max()
-
-	# Complaints by year
-	year = new_df.groupby("COMPLAINT_YEAR").size().to_frame()
-
-	# Complaints by assignment
-	assignment = new_df["ASSIGNMENT"].value_counts().to_frame()
-
-	# Print Statements
-	print()
-	print("There were ", num_complaints, "reported complaints against CPD ", 
-		"officers between ", min_year, " and ", max_year, ".")
-	print()
-	print("Below are the complaint breakdowns by year and jurisdictional", 
-		"assignment: ")
-	print()
-	print("YEAR")
-	for y in year.index:
-		print(y, ": ", year.loc[y][0])
-	print()
-	print("JURISDICTIONAL ASSIGNMENT")
-	for a in assignment.index:
-		print(a, ": ", assignment.loc[a][0])
-	print()
-
-	return new_df
-
-def reduce_to_copa_ipra(full_dataset):
-	'''
-	Subset the dataset to include only complaints assigned to COPA or IPRA
-	and only complaints that concern a single complainant, officer, and beat
-
-	Inputs:
-		full_dataset: (pandas dataframe)
-
-	Returns: pandas dataframe
-	'''
 	# Extract COPA and IPRA assignments
-	copa_ipra_df = full_dataset[full_dataset["ASSIGNMENT"].str.contains("IPRA")
-	 | full_dataset["ASSIGNMENT"].str.contains("COPA")]
+	copa_ipra_df = full_df[full_df["ASSIGNMENT"].str.contains("IPRA")
+	 | full_df["ASSIGNMENT"].str.contains("COPA")]
 
-	# HIGH LEVEL SUMMARY STATISTICS
-	# Total complaints for COPA/IPRA and full dataframes
-	num_complaints_final = copa_ipra_df.shape[0]
-	num_complaints_full = full_dataset.shape[0]
-	final_pct = num_complaints_final / num_complaints_full
+	# Create category counts for all variables
+	for race in race_cat:
+		copa_ipra_df[" ".join(("Complaint_Count:", race))] = copa_ipra_df["RACE_OF_COMPLAINANTS"].str.count(race)
 
-	# Year range
-	min_year = copa_ipra_df["COMPLAINT_YEAR"].min()
-	max_year = copa_ipra_df["COMPLAINT_YEAR"].max()
+	for sex in sex_cat:
+		copa_ipra_df[" ".join(("Complaint_Count:", sex))] = copa_ipra_df["SEX_OF_COMPLAINANTS"].str.count(sex)
 
-	# Complaints by year
-	year = copa_ipra_df.groupby("COMPLAINT_YEAR").size().to_frame()
+	for age in age_cat:
+		copa_ipra_df[" ".join(("Complaint_Count:", age))] = copa_ipra_df["AGE_OF_COMPLAINANTS"].str.count(age)
 
-	# Complaints by assignment
-	assignment = copa_ipra_df["ASSIGNMENT"].value_counts().to_frame()
+	for race in race_cat:
+		copa_ipra_df[" ".join(("Officer_Count:", race))] = copa_ipra_df["RACE_OF_INVOLVED_OFFICERS"].str.count(race)
 
-	# Complaints by current category
-	category = copa_ipra_df["CURRENT_CATEGORY"].value_counts().to_frame()
+	for sex in sex_cat:
+		copa_ipra_df[" ".join(("Officer_Count:", sex))] = copa_ipra_df["SEX_OF_INVOLVED_OFFICERS"].str.count(sex)
 
-	# Complaints by current status
-	status = copa_ipra_df["CURRENT_STATUS"].value_counts().to_frame()
+	for age in age_cat:
+		copa_ipra_df[" ".join(("Officer_Count:", age))] = copa_ipra_df["AGE_OF_INVOLVED_OFFICERS"].str.count(age)
 
-	# Complaints by finding
-	finding = copa_ipra_df["FINDING_CODE"].value_counts().to_frame()
-
-	# Print Statements
-	print()
-	print(num_complaints_final, " or ", "{:.0%}".format(final_pct), "of all", 
-		"complaints were under IPRA or COPA jurisdiction between ", 
-		min_year, " and ", max_year, ".")
-	print()
-	print("Below are the complaint breakdowns by year, category, status, and finding:")
-	print()
-	print("YEAR")
-	for y in year.index:
-		print(y, ": ", year.loc[y][0])
-	print()
-	print("CURRENT CATEGORY")
-	for c in category.index:
-		print(c, ": ", category.loc[c]["CURRENT_CATEGORY"])
-	print()
-	print("CURRENT STATUS")
-	for s in status.index:
-		print(s, ": ", status.loc[s]["CURRENT_STATUS"])
-	print()
-	print("FINDING")
-	for f in finding.index:
-		print(f, ": ", finding.loc[f]["FINDING_CODE"])
+	for yrs in force_yrs:
+		copa_ipra_df[" ".join(("Officer_Count:", yrs))] = copa_ipra_df["YEARS_ON_FORCE_OF_INVOLVED_OFFICERS"].str.count(yrs)
 
 	return copa_ipra_df
 
@@ -187,7 +110,7 @@ def top_k_complaints_by_beat(copa_ipra_df, beat, k, year = None, month = None):
 	count = complaint_counts["CURRENT_CATEGORY"].values.tolist()
 
 	# Create output list
-	output []
+	output = []
 	loop_len = min(k, len(name))
 
 	for i in range(loop_len):
@@ -196,6 +119,11 @@ def top_k_complaints_by_beat(copa_ipra_df, beat, k, year = None, month = None):
 
 	return output
 
+
+def complaint_volume_race(copa_ipra_df, year = None, month = None):
+	'''
+	'''
+	pass 
 
 ##### IPRA / COPRA COMPARISONS ####
 
